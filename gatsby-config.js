8b2766794727fe6610eda1ef4992bebc25818c47
Ruben9922 <Ruben9922@users.noreply.github.com>
2018-09-08 17:48:24 +0100

    Provide site description and keywords

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: 'Ruben Dougall',
+    description: 'Ruben Dougall. Computer Science student at University of Warwick. Recreational house music producer.',
+    keywords: 'ruben, dougall, rubendougall, personal, website, university, warwick, computer, science, student, house, music, producer',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
