393d7dd7bacd4453e6e4f5a4916d003e8f8903cc
Ruben9922 <Ruben9922@users.noreply.github.com>
2019-01-18 14:43:36 +0000

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: 'Ruben Dougall',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-23280894-7",
+        anonymize: true,
+        respectDNT: true,
+      },
+    },
+  ],
 }
