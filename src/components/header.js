59763954f81587e521d95b3db25f5d2666b70131
Ruben9922 <Ruben9922@users.noreply.github.com>
2019-12-15 18:24:20 +0000

    Change icon in app bar into button and link to index page

--- src/components/header.js
+++ src/components/header.js
@@ -6,11 +6,12 @@ import Button from "@material-ui/core/Button";
 import AppBar from "@material-ui/core/AppBar";
 import { makeStyles } from "@material-ui/core/styles";
 import Link from "@material-ui/core/Link";
+import IconButton from "@material-ui/core/IconButton";
 
 export default function Header() {
   const useStyles = makeStyles(theme => ({
     menuButton: {
-      marginRight: theme.spacing(2.5),
+      marginRight: theme.spacing(2),
     },
     title: {
       flexGrow: 1,
@@ -21,7 +22,9 @@ export default function Header() {
   return (
     <AppBar position="static">
       <Toolbar>
-        <CodeIcon edge="start" className={classes.menuButton} color="inherit"/>
+        <IconButton edge="start" className={classes.menuButton} color="inherit" href="/">
+          <CodeIcon />
+        </IconButton>
         <Typography variant="h6" className={classes.title}>
           <Link href="/" color="inherit" underline="none">
             Ruben Dougall
