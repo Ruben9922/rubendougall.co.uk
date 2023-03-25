7fca66dc364f0d188472f499a077b41c7f31dae5
Ruben Dougall <Ruben9922@users.noreply.github.com>
2023-03-25 09:56:46 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,8 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+import profileImage from "./src/images/ruben.jpg";
+
 module.exports = {
   plugins: [
     `gatsby-plugin-material-ui`,
@@ -20,10 +22,9 @@ module.exports = {
   siteMetadata: {
     title: "Ruben Dougall",
     titleTemplate: "%s – Ruben Dougall",
-    description:
-      ".NET Developer. Computer Science graduate from University of Warwick.",
+    description: "Software Engineer. MEng Computer Science graduate (Warwick).",
     url: "https://rubendougall.co.uk", // No trailing slash allowed!
-    image: "/static/ruben-532b64d7c591a5910531799fc8bc1a04.jpg", // Path to your image you placed in the 'static' folder
+    image: profileImage, // Path to your image you placed in the 'static' folder
     twitterUsername: "@ruben_dougall",
   },
 };
